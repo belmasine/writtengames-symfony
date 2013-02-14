d43f2e72beef82cb7798641914a21764c6f505c0
Moritz Kraft <chamalla.extract@gmail.com>
2013-02-14 22:48:48 +0000

    fixed layout issues, added twig extension to app bundle

---
+++ src/WrittenGames/ApplicationBundle/Twig/Extension/ApplicationExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace WrittenGames\ApplicationBundle\Twig\Extension;
+
+use Twig_Extension;
+use Twig_Function_Method;
+
+class ApplicationExtension extends Twig_Extension
+{
+    protected $container;
+
+    public function __construct( $container )
+    {
+        $this->container = $container;
+    }
+
+    public function getName()
+    {
+        return 'application_extension';
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            'foo' => new Twig_Function_Method( $this, 'foo', array(
+                'is_safe' => array( 'html' ),
+            )),
+        );
+    }
+
+    public function foo()
+    {
+        return '<i>Foo</i>';
+    }
+}
