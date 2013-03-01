cab243ffb05462d62f52347e29c3608bf2a4a652
userfriendly <chamalla.extract@gmail.com>
2013-03-01 13:33:42 +0000

    enabled connect funtionality, todo: override vendor templates in app

--- src/WrittenGames/ApplicationBundle/Security/UserProvider.php
+++ src/WrittenGames/ApplicationBundle/Security/UserProvider.php
@@ -52,29 +52,10 @@ class UserProvider implements OAuthAwareUserProviderInterface
          *  tag your event listener like so:
          *  - { name: kernel.event_listener, event: security.user_accounts_merged, method: onEvent }
          */
-        die( 'Method connect() not implemented yet' );
-        $property = $this->getProperty($response);
-        $username = $response->getUsername();
-
-        //on connect - get the access token and the user ID
-        $service = $response->getResourceOwner()->getName();
-
-        $setter = 'set'.ucfirst($service);
-        $setter_id = $setter.'Id';
-        $setter_token = $setter.'AccessToken';
-
-        //we "disconnect" previously connected users
-        if (null !== $previousUser = $this->userManager->findUserBy(array($property => $username))) {
-            $previousUser->$setter_id(null);
-            $previousUser->$setter_token(null);
-            $this->userManager->updateUser($previousUser);
-        }
-
-        //we connect current user
-        $user->$setter_id($username);
-        $user->$setter_token($response->getAccessToken());
-
-        $this->userManager->updateUser($user);
+        $this->createIdentity( $user, $response );
+        // Note:
+        // original bundle method "disconnected" previously,
+        // connected users not sure if that's really necessary
     }
 
     /**
